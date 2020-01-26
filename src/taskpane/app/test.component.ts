f1b3fa3d3990c00821e264fc2d38e26bbea4d43f
Stephen Garland <garland@csail.mit.edu>
2020-01-26 11:22:50 -0500

    No need to import CommonModule

--- src/taskpane/app/test.component.ts
+++ src/taskpane/app/test.component.ts
@@ -1,6 +1,4 @@
 import { Component } from "@angular/core";
-// eslint-disable-next-line no-unused-vars
-import { CommonModule } from "@angular/common";
 const template = require("./test.component.html");
 /* global require */
 
