cbd1321119d20b4e643fe73dc175157e7c6d4fcd
Stephen Garland <garland@csail.mit.edu>
2020-01-26 11:17:20 -0500

    Add TestComponent

--- src/taskpane/app/app.module.ts
+++ src/taskpane/app/app.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import AppComponent from "./app.component";
 import { CommonModule } from "@angular/common";
-
+import { TestComponent } from "./test.component";
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TestComponent],
   imports: [BrowserModule, CommonModule],
   bootstrap: [AppComponent]
 })
---
+++ src/taskpane/app/test.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+// eslint-disable-next-line no-unused-vars
+import { CommonModule } from "@angular/common";
+const template = require("./test.component.html");
+/* global require */
+
+@Component({
+  selector: "app-test",
+  template
+})
+export class TestComponent {
+ 
+}
