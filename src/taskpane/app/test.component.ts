90c9588cf781249fe37bbb9c5c2759506880f7c7
Steve Garland <garland@csail.mit.edu>
2020-10-21 19:24:54 -0400

    Component no longer used

--- src/taskpane/app/test.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from "@angular/core";
-/* global require */
-
-@Component({
-  selector: "app-test",
-  template: require("./test.component.html")
-})
-export class TestComponent { }
