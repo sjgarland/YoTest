358fc3c083c2c312094f433d693df95cbffbb2c2
Steve Garland <garland@csail.mit.edu>
2020-10-21 19:26:30 -0400

    TestComponent no longer exists

--- src/taskpane/app/app.module.ts
+++ src/taskpane/app/app.module.ts
@@ -2,9 +2,8 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import AppComponent from "./app.component";
 import { CommonModule } from "@angular/common";
-import { TestComponent } from "./test.component";
 @NgModule({
-  declarations: [AppComponent, TestComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, CommonModule],
   bootstrap: [AppComponent]
 })
